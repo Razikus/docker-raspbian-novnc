d74d6e7f630f578970427e908e7b433cdc89e14a
Adam Raźniewski <adam.razniewski@gmail.com>
2017-12-09 20:22:58 +0000

    tightvnc as provider

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get install git x11vnc git net-tools
 RUN apt-get install screen 
 RUN apt-get install python python3
 RUN apt-get install xvfb
+RUN apt-get install tightvncserver
 
 RUN git clone https://github.com/novnc/noVNC.git
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,2 +1,4 @@
-screen -dmS x11Server x11vnc -forever -usepw -create -passwd $X11PASSWD
+#screen -dmS x11Server x11vnc -forever -usepw -create -passwd $X11PASSWD
+export USER=root
+screen -dmS vncserver :1
 /noVNC/utils/launch.sh --vnc localhost:5900 --listen 8080
